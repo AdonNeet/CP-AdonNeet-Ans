4028edc1c25c02eb5cac95e4fa91309efaf042d1
AdonNeet <baradontap@gmail.com>
2023-07-17 18:16:29 +0700

    random

--- TOKI/troc31_A.cpp
+++ TOKI/troc31_A.cpp
@@ -1,25 +1,33 @@
 /*
  *		Author	: AdonNeet
- *		Date	: 2023-07-09 23:48:25.000-05:00
+ *		Date	: 2023-07-10 00:05:39.000-05:00
 */
  
 #pragma GCC optimize("Ofast")
 // #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma") // if the judge machine use intel
 #include <bits/stdc++.h>  
+
+ 
 using namespace std;
 
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
+
 int main()
 {
-    fast_cin();
-    int n, a, b, c;
-    cin >> n >> a >> b >> c;
+    #ifndef ONLINE_JUDGE
+    freopen("../input.in", "r", stdin);
+    freopen("../output.out", "w", stdout);
+    #endif
 
-    if(((n%a == 0) && (n%b != 0)) || n%c == 0){
-        cout << "YES";
+    fast_cin();
+    string s, t, nd;
+    cin >> s >> t;
+    nd = s + t;
+
+    if(s == t){
+        cout << s;
     }else {
-        cout << "NO";
+        cout << nd;
     }
-    
 }
